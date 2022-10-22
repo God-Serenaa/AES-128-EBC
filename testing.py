af6e3bb13524f6a765efc7d7fa3714cc4a16bd19
Mehrab Islam Arnab <aarnabofficial@gmail.com>
2022-10-22 16:13:20 +0600

    buzhte shubidha

--- testing.py
+++ testing.py
@@ -1,8 +1,8 @@
 from AES import AES128_EBC
 
 if __name__ == '__main__':
-    password = "sajid ekta bolod"
-    aes = AES128_EBC(password)
+    key = "sajid ekta bolod"
+    aes = AES128_EBC(key)
     message = "susmir biye hobe na"
     cipher_text = aes.encrypt(message)
     print(cipher_text)
